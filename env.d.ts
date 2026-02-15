47a2afb698de70fbeba702ec2681c66f0b451ab8
yu859 <15715093608@163.com>
2026-02-15 11:29:36 +0800

    fix: Pre-publish cleanup

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,14 @@
+/// <reference types="vite/client" />
+
+// CSS Houdini Paint API
+interface PaintWorklet {
+  addModule(moduleURL: string): Promise<void>;
+}
+
+declare namespace CSS {
+  var paintWorklet: PaintWorklet;
+}
+
 //定义全局变量
 declare const PROD: boolean; //生产环境
 declare const DEV: boolean; //开发环境
